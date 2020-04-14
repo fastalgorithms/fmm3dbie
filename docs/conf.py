5c1740b05878d5d2e445fe07b13c5f5a0637c340
Manas Rachh <mrachh@flatironinstitute.org>
2020-04-14 18:10:21 -0400

    adding conf

--- docs/conf.py
+++ docs/conf.py
@@ -38,7 +38,7 @@ extensions = [
     'sphinx.ext.napoleon',
     'sphinx.ext.mathjax',
 #    'sphinx.ext.autosectionlabel',   # needs v 1.4; can :ref: other files w/o this; removed 7/29/18
-    'texext',
+#    'texext',
 #    'sphinxcontrib.bibtex',
 ]
 
