058c3327e1b7149557e2c20aa15e8955f992b36a
Libin Lu <llu@flatironinstitute.org>
2023-12-03 22:19:28 -0500

    lap example integer8 works

--- examples/laplace/lap_dir_iter_example.f
+++ examples/laplace/lap_dir_iter_example.f
@@ -1,10 +1,11 @@
       implicit real *8 (a-h,o-z) 
+      implicit integer(8) (i-n)
       real *8, allocatable :: srcvals(:,:),srccoefs(:,:)
       real *8, allocatable :: wts(:)
       character *100 fname
-      integer ipars(2)
+      integer(8) ipars(2)
 
-      integer, allocatable :: norders(:),ixyzs(:),iptype(:)
+      integer(8), allocatable :: norders(:),ixyzs(:),iptype(:)
 
       real *8 xyz_out(3),xyz_in(3)
       real *8, allocatable :: sigma(:),rhs(:)
@@ -12,9 +13,9 @@
       real *8 eps_gmres
       real *8 dpars(2)
       complex * 16 zpars(3)
-      integer numit,niter
+      integer(8) numit,niter
 
-      integer ipatch_id
+      integer(8) ipatch_id
       real *8 uvs_targ(2),dtmp
 
       logical isout0,isout1
@@ -113,8 +114,8 @@ c
       allocate(sigma(npts),rhs(npts))
 
       do i=1,npts
-        call l3d_slp(xyz_out,3,srcvals(1,i),0,dpars,1,zpars,0,
-     1     ipars,rhs(i))
+        call l3d_slp(xyz_out,int(3,8),srcvals(1,i),int(0,8),dpars,
+     1     int(1,8),zpars,int(0,8),ipars,rhs(i))
         sigma(i) = 0 
       enddo
 
@@ -142,11 +143,12 @@ c
 c
 c       test solution at interior point
 c
-      call l3d_slp(xyz_out,3,xyz_in,0,dpars,1,zpars,0,ipars,potex)
+      call l3d_slp(xyz_out,int(3,8),xyz_in,int(0,8),dpars,int(1,8),
+     1             zpars,int(0,8),ipars,potex)
       pot = 0
       do i=1,npts
-        call l3d_comb(srcvals(1,i),3,xyz_in,0,dpars,3,zpars,0,ipars,
-     1     dtmp)
+        call l3d_comb(srcvals(1,i),int(3,8),xyz_in,int(0,8),dpars,
+     1     int(3,8),zpars,int(0,8),ipars,dtmp)
         pot = pot + sigma(i)*wts(i)*dtmp
       enddo
 
@@ -180,18 +182,19 @@ c
       subroutine setup_geom(igeomtype,norder,npatches,ipars, 
      1    srcvals,srccoefs,ifplot,fname)
       implicit real *8 (a-h,o-z)
-      integer igeomtype,norder,npatches,ipars(*),ifplot
+      implicit integer(8) (i-n)
+      integer(8) igeomtype,norder,npatches,ipars(*),ifplot
       character (len=*) fname
       real *8 srcvals(12,*), srccoefs(9,*)
       real *8, allocatable :: uvs(:,:),umatr(:,:),vmatr(:,:),wts(:)
 
       real *8, pointer :: ptr1,ptr2,ptr3,ptr4
-      integer, pointer :: iptr1,iptr2,iptr3,iptr4
+      integer(8), pointer :: iptr1,iptr2,iptr3,iptr4
       real *8, target :: p1(10),p2(10),p3(10),p4(10)
       real *8, allocatable, target :: triaskel(:,:,:)
       real *8, allocatable, target :: deltas(:,:)
-      integer, allocatable :: isides(:)
-      integer, target :: nmax,mmax
+      integer(8), allocatable :: isides(:)
+      integer(8), target :: nmax,mmax
 
       procedure (), pointer :: xtri_geometry
 
@@ -294,11 +297,11 @@ c  identity for the flux due to a point charge
 c
 c
 c  input:
-c    npatches - integer
+c    npatches - integer(8)
 c       number of patches
-c    norder - integer
+c    norder - integer(8)
 c       order of discretization
-c    npts - integer
+c    npts - integer(8)
 c       total number of discretization points on the surface
 c    srccoefs - real *8 (9,npts)
 c       koornwinder expansion coefficients of geometry info
@@ -311,16 +314,16 @@ c      whether the target is in the interior or not
 c
 
       implicit none
-      integer npatches,norder,npts,npols
+      integer(8) npatches,norder,npts,npols
       real *8 srccoefs(9,npts),srcvals(12,npts),xyzout(3),wts(npts)
       real *8 tmp(3)
       real *8 dpars,done,pi
       real *8, allocatable :: rsurf(:),err_p(:,:) 
-      integer ipars,norderhead,nd
+      integer(8) ipars,norderhead,nd
       complex *16 zk
       real *8 val
 
-      integer ipatch,j,i
+      integer(8) ipatch,j,i
       real *8 ra,ds
       logical isout
 
@@ -339,8 +342,8 @@ c
       do ipatch=1,npatches
         do j=1,npols
           i = (ipatch-1)*npols + j
-          call l3d_sprime(xyzout,12,srcvals(1,i),0,dpars,1,zk,0,ipars,
-     1       val)
+          call l3d_sprime(xyzout,int(12,8),srcvals(1,i),int(0,8),dpars,
+     1       int(1,8),zk,int(0,8),ipars,val)
 
           call cross_prod3d(srcvals(4,i),srcvals(7,i),tmp)
           ds = sqrt(tmp(1)**2 + tmp(2)**2 + tmp(3)**2)
