a781c5eea89f1b4df63a27a12b1896a8fa21ecd6
Libin Lu <llu@flatironinstitute.org>
2024-04-04 17:36:14 -0400

    add some comments in helm_comb_dir

--- src/helm_wrappers/helm_comb_dir.f
+++ src/helm_wrappers/helm_comb_dir.f
@@ -1,3 +1,46 @@
+cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+c
+c   Combined field representation for acoustic scattering
+c   from sound hard obstacles
+c
+c  PDE:
+c    (\Delta + k^2) u = 0
+c
+c  Boundary conditions:
+c    u = f
+c
+c  Representation:
+c    u = (\alpha S_{k}  + \beta D_{k})[\sigma]
+c
+c  Integral equations obtained by imposing:
+c    u = f
+c
+c  and is given by:
+c    z \sigma + (\alpha S_{k}  + \beta D_{k})[\sigma] = f
+c
+c  where:
+c    z = -1/2 beta for interior problem
+c      =  1/2 beta for exterior problem
+c
+cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+c
+c  User callable routines:
+c    - helm_comb_dir_solver:
+c    - helm_comb_dir_eval:
+c
+cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+c
+c  Advanced interfaces:
+c    - getnearquad_helm_comb_dir: self
+c    - getnearquad_helm_comb_dir_eval: targ
+c    - lpcomp_helm_comb_dir_addsub: self -> eval_addsub
+c    - lpcomp_helm_comb_dir_eval_addsub: targ + self
+c    - helm_comb_dir_solver_guru:
+c    - helm_comb_dir_solver_memest:
+c
+c
+c
+c
 c
 c
 c     This file contains the following user callable
@@ -81,7 +124,197 @@ c
 
 
 
-      subroutine getnearquad_helm_comb_dir(npatches,norders,
+
+       subroutine getnearquad_helm_comb_dir(npatches,norders,
+     1   ixyzs,iptype,npts,srccoefs,srcvals,ndtarg,ntarg,targs,
+     2   ipatch_id,uvs_targ,eps,zpars,iquadtype,nnz,row_ptr,col_ind,
+     3   iquad,rfac0,nquad,wnear)
+c
+c       this subroutine generates the near field quadrature
+c       for the representation u = (\alpha S_{k}  + \beta D_{k}) ---(1)
+c       where the near field is specified by the user
+c       in row sparse compressed format.
+c
+c       The quadrature is computed by the following strategy
+c        targets within a sphere of radius rfac0*rs
+c        of a chunk centroid is handled using adaptive integration
+c        where rs is the radius of the bounding sphere
+c        for the patch
+c
+c       All other targets in the near field are handled via
+c        oversampled quadrature
+c
+c       The recommended parameter for rfac0 is 1.25d0
+c
+c       input:
+c         npatches - integer
+c            number of patches
+c
+c         norders - integer(npatches)
+c            order of discretization on each patch
+c
+c         ixyzs - integer(npatches+1)
+c            starting location of data on patch i
+c
+c         iptype - integer(npatches)
+c           type of patch
+c           iptype = 1 -> triangular patch discretized with RV nodes
+c
+c         npts - integer
+c            total number of discretization points on the boundary
+c
+c         srccoefs - real *8 (9,npts)
+c            koornwinder expansion coefficients of xyz, dxyz/du,
+c            and dxyz/dv on each patch.
+c            For each point srccoefs(1:3,i) is xyz info
+c                           srccoefs(4:6,i) is dxyz/du info
+c                           srccoefs(7:9,i) is dxyz/dv info
+c
+c          srcvals - real *8 (12,npts)
+c             xyz(u,v) and derivative info sampled at the
+c             discretization nodes on the surface
+c             srcvals(1:3,i) - xyz info
+c             srcvals(4:6,i) - dxyz/du info
+c             srcvals(7:9,i) - dxyz/dv info
+c             srcvals(10:12,i) - normals info
+c
+c         ndtarg - integer
+c            leading dimension of target array
+c
+c         ntarg - integer
+c            number of targets
+c
+c         targs - real *8 (ndtarg,ntarg)
+c            target information
+c
+c         ipatch_id - integer(ntarg)
+c            id of patch of target i, id = -1, if target is off-surface
+c
+c         uvs_targ - real *8 (2,ntarg)
+c            local uv coordinates on patch if on surface, otherwise
+c            set to 0 by default
+c
+c          eps - real *8
+c             precision requested
+c
+c          zpars - complex *16 (3)
+c              kernel parameters (Referring to formula (1))
+c              zpars(1) = k
+c              zpars(2) = alpha
+c              zpars(3) = beta
+c
+c           iquadtype - integer
+c              quadrature type
+c              iquadtype = 1, use ggq for self + adaptive integration
+c                 for rest
+c
+c
+c           nnz - integer
+c             number of source patch-> target interactions in the near field
+c
+c           row_ptr - integer(ntarg+1)
+c              row_ptr(i) is the pointer
+c              to col_ind array where list of relevant source patches
+c              for target i start
+c
+c           col_ind - integer (nnz)
+c               list of source patches relevant for all targets, sorted
+c               by the target number
+c
+c           iquad - integer(nnz+1)
+c               location in wnear array where quadrature for col_ind(i)
+c               starts
+c
+c           rfac0 - integer
+c               radius parameter for near field
+c
+c           nquad - integer
+c               number of entries in wnear
+c
+c        output
+c            wnear - complex *16(nquad)
+c               the desired near field quadrature
+c
+c
+
+      implicit none
+      integer, intent(in) :: npatches,norders(npatches),npts,nquad
+      integer, intent(in) :: ixyzs(npatches+1),iptype(npatches)
+      real *8, intent(in) :: srccoefs(9,npts),srcvals(12,npts),eps
+      real *8, intent(in) :: rfac0
+      integer, intent(in) :: ndtarg,ntarg
+      integer, intent(in) :: iquadtype
+      real *8, intent(in) :: targs(ndtarg,ntarg)
+      integer, intent(in) :: ipatch_id(ntarg)
+      real *8, intent(in) :: uvs_targ(2,ntarg)
+      complex *16, intent(in) :: zpars(3)
+      integer, intent(in) :: nnz
+      integer, intent(in) :: row_ptr(ntarg+1),col_ind(nnz),iquad(nnz+1)
+      complex *16, intent(out) :: wnear(nquad)
+
+
+      integer ipars
+      integer ndd,ndz,ndi
+      real *8 dpars
+
+      complex *16 alpha,beta
+      integer i,j
+      integer ipv
+
+      procedure (), pointer :: fker
+      external h3d_slp, h3d_dlp, h3d_comb
+
+c
+c
+c        initialize the appropriate kernel function
+c
+
+      alpha = zpars(2)
+      beta = zpars(3)
+
+      ndz = 3
+      ndi = 0
+      ndd = 0
+      if(iquadtype.eq.1) then
+        fker => h3d_comb
+        ipv = 1
+        if(abs(alpha).ge.1.0d-16.and.abs(beta).lt.1.0d-16) then
+          fker=>h3d_slp
+          ipv = 0
+        else if(abs(alpha).lt.1.0d-16.and.abs(beta).ge.1.0d-16) then
+          fker=>h3d_dlp
+        endif
+
+
+        call zgetnearquad_ggq_guru(npatches,norders,ixyzs,
+     1     iptype,npts,srccoefs,srcvals,ndtarg,ntarg,targs,
+     1     ipatch_id,uvs_targ,eps,ipv,fker,ndd,dpars,ndz,zpars,
+     1     ndi,ipars,nnz,row_ptr,col_ind,iquad,rfac0,nquad,wnear)
+      endif
+
+
+      if(abs(alpha).ge.1.0d-16.and.abs(beta).lt.1.0d-16) then
+C$OMP PARALLEL DO DEFAULT(SHARED)
+        do i=1,nquad
+          wnear(i) = wnear(i)*alpha
+        enddo
+C$OMP END PARALLEL DO
+      else if(abs(alpha).lt.1.0d-16.and.abs(beta).ge.1.0d-16) then
+C$OMP PARALLEL DO DEFAULT(SHARED)
+        do i=1,nquad
+          wnear(i) = wnear(i)*beta
+        enddo
+C$OMP END PARALLEL DO
+      endif
+
+      return
+      end
+c
+c
+c
+
+
+      subroutine getnearquad_helm_comb_dir_eval(npatches,norders,
      1   ixyzs,iptype,npts,srccoefs,srcvals,ndtarg,ntarg,targs,
      2   ipatch_id,uvs_targ,eps,zpars,iquadtype,nnz,row_ptr,col_ind,
      3   iquad,rfac0,nquad,wnear)
@@ -455,7 +688,7 @@ C$OMP END PARALLEL DO
 
       iquadtype = 1
 
-      call getnearquad_helm_comb_dir(npatches,norders,
+      call getnearquad_helm_comb_dir_eval(npatches,norders,
      1      ixyzs,iptype,npts,srccoefs,srcvals,ndtarg,ntarg,targs,
      1      ipatch_id,uvs_targ,eps,zpars,iquadtype,nnz,row_ptr,col_ind,
      1      iquad,rfac0,nquad,wnear)
@@ -465,7 +698,7 @@ c
 c
 c   compute layer potential
 c
-      call lpcomp_helm_comb_dir_addsub_guru(npatches, norders, ixyzs,
+      call lpcomp_helm_comb_dir_eval_addsub(npatches, norders, ixyzs,
      1  iptype, npts, srccoefs, srcvals, ndtarg, ntarg, targs,
      2  eps, 0, dpars, 3, zpars, 0, ipars,
      2  nnz, row_ptr, col_ind, iquad, nquad, 1, wnear, novers,
@@ -657,7 +890,7 @@ c
       real *8 ttot,done,pi
 
 
-      call lpcomp_helm_comb_dir_addsub_guru(npatches, norders, ixyzs,
+      call lpcomp_helm_comb_dir_eval_addsub(npatches, norders, ixyzs,
      1  iptype, npts, srccoefs, srcvals, 12, npts, srcvals,
      2  eps, ndd, dpars, ndz, zpars, ndi, ipars,
      2  nnz, row_ptr, col_ind, iquad, nquad, nker, wnear, novers,
@@ -670,7 +903,7 @@ c
 c
 c
 c
-      subroutine lpcomp_helm_comb_dir_addsub_guru(npatches, norders,
+      subroutine lpcomp_helm_comb_dir_eval_addsub(npatches, norders,
      1  ixyzs,
      1  iptype, npts, srccoefs, srcvals, ndtarg, ntarg, targs,
      2  eps, ndd, dpars, ndz, zpars, ndi, ipars,
