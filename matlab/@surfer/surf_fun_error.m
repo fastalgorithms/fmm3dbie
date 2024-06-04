6d5550cb7952bb28af44ff141d4c25a0c50a3922
jghoskins <32715900+jghoskins@users.noreply.github.com>
2024-06-04 10:21:53 -0500

    addition of a few helper routines
    still in beta

---
+++ matlab/@surfer/surf_fun_error.m
@@ -0,0 +1,91 @@
+function [errps] = surf_fun_error(obj,fun,p)
+
+%%%
+%
+%       This routine computes an estimate of the approximation error
+%       in a collection of functions defined on a surfer object.
+%
+%       Inputs:
+%       obj     --- the surfer object
+%       fun     --- (nfuns x npts) array containing function values
+%                   at the points in obj
+%       p       --- optional: the norm to use to measure the coeffs
+%                   (default is infinity)
+%
+%       .   .   .  
+%       
+%       iptype = 1 : Koornwinder (look at last norder)
+%       iptype = 11: Gauss-Legendere (look at upper triangle > norder)
+%       iptype = 12: Chebyshev (look at upper triangle > norder)
+%
+%%%
+
+    npatches = obj.npatches;
+
+%%%%%%%%%%%%%%
+
+    ntmp = zeros(npatches,2);
+    ntmp(:,1) = obj.norders;
+    ntmp(:,2) = obj.iptype;
+
+    [fcoefs] = vals_to_coefs_surface_fun(obj, fun);
+    fcoefs = fcoefs.';
+    nfuns = size(fcoefs,2);
+
+    errps = zeros(npatches,nfuns);
+    parea = zeros(npatches,1);
+    [ntmp_uni,~,intmp] = unique(ntmp,'rows');
+    nuni = size(ntmp_uni,1);
+    umats = cell(nuni,1);
+    for i=1:nuni
+        norder = ntmp_uni(i,1);
+        iptype = ntmp_uni(i,2);
+
+        inds = find(intmp == i);
+        indv = obj.ixyzs(inds);
+
+        if (iptype == 1)
+            norderhead = norder-1;
+            i1    = (norderhead+1)*(norderhead+2)/2;
+            npols = (norder+1)*(norder+2)/2;
+            ipols = (i1+1):npols;
+        end
+        if (iptype == 11)
+            nords1  = 0:norder;
+            nords2  = 0:norder;
+            [N1,N2] = ndgrid(nords1,nords2);
+            degs = N1(:) + N2(:);
+            ipols = find(degs > norder);
+        end
+        if (iptype == 12)
+            nords1  = 0:norder;
+            nords2  = 0:norder;
+            [N1,N2] = ndgrid(nords1,nords2);
+            degs = N1(:) + N2(:);
+            ipols = find(degs > norder);
+        end
+
+        [I,V] = meshgrid(indv,ipols);
+        itot = I(:)+V(:)-1;
+        cftmp = reshape(fcoefs(itot,:),numel(ipols),[]);
+        if (nargin >2)
+            nrms = vecnorm(cftmp,p,1);
+        else
+            nrms = vecnorm(cftmp,Inf,1);
+        end
+        errs = reshape(nrms,[],nfuns);
+        errps(inds,:) = errs;
+
+        areas = sum([obj.weights{inds}],1);
+        parea(inds) = areas;
+    end
+    
+    if (nargin >2)
+        a     = area(obj);
+        errps = (parea/a).^(1/p).*errps;
+    end
+
+    errps = errps.';
+
+
+end
---
+++ matlab/@surfer/vals2coefs.m
@@ -0,0 +1,63 @@
+function [coefs] = vals2coefs(obj,vals)
+%
+%  Oversamples the given geometry given a current discretization
+%  and a patchwise oversampling parameter, or a common oversampling
+%  order for all patches
+%
+    npatches = obj.npatches;
+    npts = obj.npts;
+    
+    vals = vals.';
+    if (size(vals,1) ~= npts)
+        disp("error in size");
+        coefs = [];
+        return
+    end
+    
+    ikrn = find(obj.iptype == 1);
+    ileg = find(obj.iptype == 11);
+    iche = find(obj.iptype == 12);
+
+%%%%%%%%%%%%%%
+%       .   .   .   first the koornwinder
+
+    nords = obj.norders(ikrn);
+
+
+%%%%%%%%%%%%%%
+
+    ntmp = zeros(npatches,2);
+    ntmp(:,1) = obj.norders;
+    ntmp(:,2) = obj.iptype;
+    
+    
+    coefs = zeros(size(vals));
+    
+    [ntmp_uni,~,intmp] = unique(ntmp,'rows');
+    nuni = size(ntmp_uni,1);
+    umats = cell(nuni,1);
+    for i=1:nuni
+        norder = ntmp_uni(i,1);
+        iptype = ntmp_uni(i,2);
+        if(iptype == 1)
+            rnodes = koorn.rv_nodes(norder);
+            umats{i} = koorn.vals2coefs(norder,rnodes);       
+        elseif(iptype == 11)
+            rnodes = polytens.lege_nodes(norder);
+            umats{i} = polytens.lege_vals2coefs(norder,rnodes);
+        elseif(iptype == 12)
+            rnodes = polytens.cheb_nodes(norder);
+            umats{i} = polytens.cheb_vals2coefs(norder,rnodes);
+        end
+        npp  = size(umats{i},2);
+        inds = (intmp == i);
+        indv = obj.ixyzs(inds);
+        [I,V] = meshgrid(indv,1:npp);
+        itot = I(:)+V(:)-1;
+        cftmp = umats{i}*reshape(vals(itot,:),npp,[]);
+        coefs(itot,:) = reshape(cftmp,[],size(vals,2));
+    end
+    
+    coefs = coefs.';
+    
+end
