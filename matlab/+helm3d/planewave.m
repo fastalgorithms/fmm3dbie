5bd657f15a26c1afa7990f84f5d34159429ec830
Manas Rachh <mrachh@flatironinstitute.org>
2024-06-08 12:39:04 -0400

    fixing plane wave scaling

--- matlab/+helm3d/planewave.m
+++ matlab/+helm3d/planewave.m
@@ -4,7 +4,12 @@ function [u, gradu] = planewave(zk, dir, targinfo)
 %
 % Input arguments:
 %   zk - wave number
-%   dir(3) - incident planewave direction (should be a unit vector)
+%   dir(2) or dir(3) - incident plane wave direction
+%     if it is a two vector then dir = (theta, phi), 
+%     d = (sin(theta)*cos(phi), sin(theta)*sin(phi), cos(theta))
+%
+%     if ir is a three vector then
+%     d = (dir(1), dir(2), dir(3))/norm(dir) 
 %   targinfo - either (3,n) array or a struct
 %      with field targinfo.r which is a (3,n) array
 %   
@@ -18,14 +23,26 @@ function [u, gradu] = planewave(zk, dir, targinfo)
     if n1~=3
       error('HELM3D.planewave: invalid size of target array');
     end
-    uexp = exp(1i*zk*sum(dir(:).*targs));
+
+    if length(dir(:)) == 2
+       thet = dir(1);
+       phi = dir(2);
+       d = [sin(thet)*cos(phi),; sin(thet)*sin(phi); cos(thet)]
+    elseif length(dir(:)) == 3
+       rn = sqrt(dir(1)^2 + dir(2)^2 + dir(3)^3);
+       d = dir(:);
+       d = d/rn;
+    else
+      error('HELM3D.planewave: invalid size of direction vector');
+    end
+    uexp = exp(1i*zk*sum(d.*targs));
     
     u = uexp.';
     if nargout >= 2
         gradu = complex(zeros(3,n2));
-        gradu(1,:) = 1i*zk*dir(1)*uexp;
-        gradu(2,:) = 1i*zk*dir(2)*uexp;
-        gradu(3,:) = 1i*zk*dir(3)*uexp;
+        gradu(1,:) = 1i*zk*d(1)*uexp;
+        gradu(2,:) = 1i*zk*d(2)*uexp;
+        gradu(3,:) = 1i*zk*d(3)*uexp;
     end
     
 
